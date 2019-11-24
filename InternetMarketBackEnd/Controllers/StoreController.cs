10873dc9c3bb92eb0132c7843a808720bc30084b
admin <admin@corp>
2019-11-24 14:07:33 +0300

    Change logical in store controller

--- InternetMarketBackEnd/Controllers/StoreController.cs
+++ InternetMarketBackEnd/Controllers/StoreController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetMarketBackEnd.Application.Interfaces;
 using InternetMarketBackEnd.Controllers.Common;
 using InternetMarketBackEnd.Domain.Entity;
 using Microsoft.AspNetCore.Http;
@@ -12,25 +13,43 @@ namespace InternetMarketBackEnd.Controllers
     [Route("[controller]")]
     public class StoreController : BaseApiController, IStoreController
     {
-        //private IOrderAppService
+        private readonly IOrderAppService _orderAppService;
+
+
+        public StoreController(IOrderAppService orderAppService)
+        {
+            if (orderAppService == null)
+                throw new ArgumentNullException("IOrderAppService");
+            _orderAppService = orderAppService;
+        }
+
+        [HttpDelete]
         public IActionResult Delete(long id)
         {
             throw new NotImplementedException();
         }
 
+        [HttpGet]
         public IActionResult GetOrderById(long id)
         {
             throw new NotImplementedException();
         }
 
+        [HttpPost]
         public IActionResult Post(Order order)
         {
             throw new NotImplementedException();
         }
 
+        [HttpPut]
         public IActionResult Put(Order id)
         {
             throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
         }
     }
 }
