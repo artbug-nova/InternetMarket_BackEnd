9c544a1b6a83c1302e92e0df945bffa7065e675a
admin <admin@corp>
2019-11-24 13:56:54 +0300

    Add IOrderService

--- InternetMarketBackEnd/Controllers/StoreController.cs
+++ InternetMarketBackEnd/Controllers/StoreController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using InternetMarketBackEnd.Controllers.Common;
+using InternetMarketBackEnd.Domain.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InternetMarketBackEnd.Controllers
 {
     [Route("[controller]")]
-    public class StoreController : BaseApiController
+    public class StoreController : BaseApiController, IStoreController
     {
         //private IOrderAppService
+        public IActionResult Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetOrderById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Post(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Put(Order id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
