52e528a54d8d4ac8bc4d0c16fb3ee14e0abefbf7
admin <admin@admin>
2019-11-29 20:30:11 +0300

    Change name application in Swagger

--- InternetMarketBackEnd/Configuration/SwaggerConfig.cs
+++ InternetMarketBackEnd/Configuration/SwaggerConfig.cs
@@ -16,7 +16,7 @@ namespace InternetMarketBackEnd.Configuration
         {
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Internet Swagger", Version = "v1" });
             });
 
 
