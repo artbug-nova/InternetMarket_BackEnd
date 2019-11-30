4605aed00190d5068d94de3115adad48b08e9864
admin <admin@admin>
2019-11-30 08:03:23 +0300

    Add middleware to project

---
+++ InternetMarketBackEnd.Application/Interfaces/IResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternetMarketBackEnd.Application.Interfaces
+{
+    public interface IResult
+    {
+        int Code { get; set; }
+        string Message { get; set; }
+        Exception Exception { get; set; }
+    }
+}
--- InternetMarketBackEnd/Middlewares/ErrorHandlingMiddleware.cs
+++ InternetMarketBackEnd/Middlewares/ErrorHandlingMiddleware.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using InternetMarketBackEnd.Application.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace InternetMarketBackEnd.Middlewares
 {
+    public static class CustomExceptionMiddleware
+    {
+        public static void UseCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+        }
+    }
     public class ErrorHandlingMiddleware
     {
         private readonly RequestDelegate _next;
@@ -15,5 +26,29 @@ namespace InternetMarketBackEnd.Middlewares
         }
 
 
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch(Exception exp)
+            {
+                _logger.LogError(exp.Message);
+                await HandleExceptionAsync(httpContext, exp);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            var error = new Result
+            {
+                Code = 500,
+                Message = "500 Error"
+            };
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = error.Code;
+            return httpContext.Response.WriteAsync(error.Message);
+        }
     }
 }
