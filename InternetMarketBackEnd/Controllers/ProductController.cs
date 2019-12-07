25b5258ac3ed535ab008aeec47a29f4416c41379
admin <admin@admin>
2019-12-07 08:28:29 +0300

    Add product controller

---
+++ InternetMarketBackEnd/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using InternetMarketBackEnd.Application.Interfaces;
+using InternetMarketBackEnd.Domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarketBackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductAppService _appService;
+        public ProductController(IProductAppService appService)
+        {
+            _appService = appService;
+        }
+        public IActionResult Add()
+        {
+            _appService.Add(new Product
+            {
+                Name = "пылесос",
+                Description = "Super pile",
+                Price = 11
+            });
+            return Ok();
+        }
+    }
+}
