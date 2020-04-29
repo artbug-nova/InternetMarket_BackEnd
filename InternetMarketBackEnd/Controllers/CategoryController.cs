f20754059924a35eddff51314b897ba94e76cc26
bishop777-sys <bishop_777@rambler.ru>
2020-04-29 14:21:24 +0300

    Implements CRUD in category controller and add Category entity

--- InternetMarketBackEnd/Controllers/CategoryController.cs
+++ InternetMarketBackEnd/Controllers/CategoryController.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using InternetMarketBackEnd.Controllers.Common;
+using InternetMarketBackEnd.Domain.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InternetMarketBackEnd.Controllers
 {
-    public class CategoryController : BaseApiController
+    public class CategoryController : BaseApiController, ICRUDController<Category>
     {
+        public Task<IActionResult> Add(Category order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetOrderById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Update(Category id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
