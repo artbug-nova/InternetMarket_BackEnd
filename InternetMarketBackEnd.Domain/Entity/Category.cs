be80ed1eb2e3fcf474116cb174275bb76276a2bc
bishop777-sys <bishop_777@rambler.ru>
2020-04-30 11:35:25 +0300

    Add category entity to project

--- InternetMarketBackEnd.Domain/Entity/Category.cs
+++ InternetMarketBackEnd.Domain/Entity/Category.cs
@@ -6,5 +6,8 @@ namespace InternetMarketBackEnd.Domain.Entity
 {
     public class Category
     {
+        public int Id { get; set; }
+        public String Name { get;set; }
+        public int? ParentId { get; set; }
     }
 }
